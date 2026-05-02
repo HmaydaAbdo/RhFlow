a0a7af1fbfa08e5c46f2585aab68872b87b96296
Abd essamad Hmayda <abdoessamadhmayda@gmail.com>
2026-05-02 14:38:55 +0100

    Enable Async programming

--- src/main/java/com/hrflow/RhErpSystemApplication.java
+++ src/main/java/com/hrflow/RhErpSystemApplication.java
@@ -3,11 +3,13 @@ package com.hrflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @EnableScheduling
+@EnableAsync
 public class RhErpSystemApplication {
 
 	public static void main(String[] args) {
